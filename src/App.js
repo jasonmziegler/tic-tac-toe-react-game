a7bf8f285259861d0a9e3d0c0468eca0a145f036
Jason Ziegler <jason@insightfulautomation.com>
2024-09-26 08:40:25 -0500

    add computer take turn

--- src/App.js
+++ src/App.js
@@ -13,13 +13,15 @@ function App() {
   let [playerMovesArray, setPlayerMovesArray] = useState(emptyArray);
   let [position, setPosition] = useState(1);
   let [gamePiece, setGamePiece] = useState("X");
-  
+  // let [computerThinking, setComputerThinking] = useState(false);
+  let computerThinking = false;
 
   function handleSubmit(e) {
     e.preventDefault();
-
+    let newArray = [...playerMovesArray];
     console.log("Form Submitted, Player played a turn");
     let index = position - 1;
+    let tempToken = gamePiece;
 
     // check for valid input
     if (position < 0 || position > 9 ) {
@@ -28,19 +30,54 @@ function App() {
       return;
     }
 
-    if (playerMovesArray[index] !== "-") {
+    if (newArray[index] !== "-") {
       alert("Space taken, please choose and empty space.");
       setPosition("");
       return;
     }
-    let newArray = [...playerMovesArray];
-
+    
     // don't need splice can just set array directly with index
     //newArray.splice(index, 1, gamePiece);
-    newArray[index] = gamePiece;
+    newArray[index] = tempToken;
     setPlayerMovesArray(newArray);
     setPosition("");
-    setGamePiece(gamePiece === "X" ? "O" : "X");
+    // TODO: check win condition
+    // if no win condition check for valids "Cat's Game"
+    // if open move and no win condition
+    // switch players
+    // setGamePiece(gamePiece === "X" ? "O" : "X";); 
+    tempToken = (tempToken === "X") ? "O" : "X";
+    // How to deal with full array no moves left? array.includes(value)
+    //TODO: this should no longer be necessary as we check for open moves in After X move
+    if (newArray.includes("-")) {
+      // O players turn (computer)
+      // TODO: remove and then reuse the position variable
+      computerThinking = true;
+      // while computerThinking true calculate positions?
+      while (computerThinking) {
+          // computer chooses a random position
+        position = Math.floor(Math.random() * 9);
+        console.log(position);
+        index = position;
+        // simulate computer thinking
+        setTimeout(100);
+
+        //check if position is empty 
+        console.log("computer choice: ",newArray[index])
+        if (newArray[index] === "-") {
+          // if empty update position with gamepiece
+          newArray[index] = tempToken; 
+          // set computer thinking to false / break while loop
+          computerThinking = false;
+        }
+      }   
+    } else {
+      console.log("No moves left")
+    }
+    setPlayerMovesArray(newArray);
+    setGamePiece(tempToken === "X" ? "O" : "X"); 
+    // check win condition
+    // check if any valid moves
   }
 
   const row1 = playerMovesArray.slice(0,3).join(" ");
@@ -60,7 +97,7 @@ function App() {
       </div>
       <div id="playerConsole">
         <h2>playerConsole</h2>
-        <p>It's {gamePiece}'s move. </p>
+        <p>It's {gamePiece}'s move. {!computerThinking ? "" : "Computer Thinking"} </p>
         <form onSubmit={handleSubmit}>
           <label htmlFor="position">Position 1-9</label>
           <input 
