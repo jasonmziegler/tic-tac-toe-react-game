9ff2c7c1cc7d727a62403110bc14c80dc3a5945e
Jason Ziegler <jason@insightfulautomation.com>
2024-09-28 15:30:35 -0500

    remove handleSubmit and playerConsole form

--- src/components/PlayerConsole.js
+++ src/components/PlayerConsole.js
@@ -3,26 +3,9 @@ import React from "react";
 function PlayerConsole({ gameState, setGameState, computerThinking, handleSubmit}) {
   return (
     <div id="playerConsole">
-        {/* <h2>playerConsole</h2> */}
-        <p>It's {gameState.gamePiece}'s move. {!computerThinking ? "" : "Computer Thinking"} </p>
-        {/* <form onSubmit={handleSubmit}>
-          <label htmlFor="position">Position 1-9</label>
-          <input 
-          type="number"
-          min="1"
-          max="9"
-          value={gameState.position}
-          id="position"
-          onChange={(e) => setGameState(
-            prevState =>({
-              ...prevState,
-              position: e.target.value,
-            })
-          )}
-          />
-          <button>End Turn</button>
-        </form> */}
-      </div>
+      <p>It's {gameState.gamePiece}'s move. {!computerThinking ? "" : "Computer Thinking"} </p>    
+      <p>Click a Square</p>
+    </div>
   )
 }
 
